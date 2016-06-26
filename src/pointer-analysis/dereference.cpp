8e2ee915c84b1f2c0d721f5d40f453ae20257a2c
Michael Tautschnig <tautschn@amazon.com>
2016-06-26 13:33:53 +0000

    Pointer dereferencing of integers is invalid
    This may arise through typecast variables.

--- src/pointer-analysis/dereference.cpp
+++ src/pointer-analysis/dereference.cpp
@@ -513,7 +513,10 @@ exprt dereferencet::dereference_plus(
     std::swap(pointer, integer);
   }
   else if(!has_pointer(pointer, ns))
-    throw "missing pointer in pointer arithmetic";
+  {
+    invalid_cond.make_true();
+    return nil_exprt();
+  }
 
   exprt size=size_of_expr(char_type(), ns);
   assert(size.is_not_nil());
