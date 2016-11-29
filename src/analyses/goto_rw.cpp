31eef046562b593c505ed209201f730104b96d91
Michael Tautschnig <tautschn@amazon.com>
2016-11-29 14:42:18 +0000

    Revert "Fixed some array and pointer reads\writes not appearing"
    This reverts commit 55d129761bd06033af036e1585cdf411782509d2.
    
    Pointers require dereferencing, and taking an address of an object does
    not induce any reads on the object.

--- src/analyses/goto_rw.cpp
+++ src/analyses/goto_rw.cpp
@@ -206,8 +206,6 @@ void rw_range_sett::get_objects_dereference(
 {
   const exprt &pointer=deref.pointer();
   get_objects_rec(READ, pointer);
-  if(mode!=READ)
-    get_objects_rec(mode, pointer);
 }
 
 /*******************************************************************\
@@ -586,14 +584,13 @@ Function: rw_range_sett::get_objects_address_of
 
 void rw_range_sett::get_objects_address_of(const exprt &object)
 {
-  if(object.id()==ID_string_constant ||
+  if(object.id()==ID_symbol ||
+     object.id()==ID_string_constant ||
      object.id()==ID_label ||
      object.id()==ID_array ||
      object.id()=="NULL-object")
     // constant, nothing to do
     return;
-  else if(object.id()==ID_symbol)
-    get_objects_rec(READ, object);
   else if(object.id()==ID_dereference)
     get_objects_rec(READ, object);
   else if(object.id()==ID_index)
@@ -756,11 +753,6 @@ void rw_range_sett::get_objects_rec(
           expr.id()==ID_string_constant)
   {
     // dereferencing may yield some weird ones, ignore these
-  }
-  else if(mode==LHS_W)
-  {
-    forall_operands(it, expr)
-      get_objects_rec(mode, *it);
   }
   else
     throw "rw_range_sett: assignment to `"+expr.id_string()+"' not handled";
