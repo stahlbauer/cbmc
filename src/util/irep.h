e8b6b2122389c623ace8d74598e9d03731f351ac
Michael Tautschnig <michael.tautschnig@qmul.ac.uk>
2016-12-26 12:30:50 +0000

    Avoid repeated hash computation, reduce irept::dt size
    On SV-COMP benchmarks, hashing accounts for >22% of CPU time (with
    profiling enabled).

--- src/util/irep.h
+++ src/util/irep.h
@@ -16,9 +16,9 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #define USE_DSTRING
 #define SHARING
-// #define HASH_CODE
+#define HASH_CODE
 #define USE_MOVE
-// #define SUB_IS_LIST
+#define SUB_IS_LIST
 
 #ifdef SUB_IS_LIST
 #include <list>
